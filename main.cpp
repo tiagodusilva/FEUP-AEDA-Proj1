5ad90d41db958aeebc706dd0f8f70a519db8ee82
Jtp <joaolucassilvamartins@gmail.com>
2019-10-30 16:58:26 +0000

    Nothing to see here, scroll up

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <gtest/gtest.h>
 #include <gtest/gtest.h>
 
@@ -8,36 +7,3 @@ int main(int argc, char* argv[]) {
     testing::GTEST_FLAG(filter) = "menu*";
     return RUN_ALL_TESTS();
 }
-=======
-#include <iostream>
-#include "include/date.h"
-#include "include/cards.h"
-#include "include/time.h"
-
-using namespace std;
-
-int
-main()
-{
-	//IndividualCard a("abc", "123", 123, Date(), Address());
-	//Date a;
-	//Time a(11, 12);
-	//cout << a << '\n';
-
-	//ofstream myfile;
-	//myfile.open("output_test.txt");
-	//myfile << a;
-	//myfile.close();
-
-	Time b;
-
-	ifstream myfile2;
-	myfile2.open("output_test.txt");
-	myfile2 >> b;
-	myfile2.close();
-
-	cout << b << '\n';
-	return 0;
-}
-
->>>>>>> ce594b381feb2b4a1091b73eebd1dddaf6a66700
